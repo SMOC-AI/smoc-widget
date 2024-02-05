ed9905932a80af753807a55e29570648103c1c40
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2024-02-05 14:38:44 +0000

    Reformat

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
+
+
 function createChatIcon(hexColor: string): HTMLElement {
     const iconElement = document.createElement('div');
     iconElement.id = 'smoc-chat-icon';
     // Additional code to set up the icon, including setting the color
     return iconElement;
-}
+  }
+  
+  // Add the icon to the document body
+  document.body.appendChild(createChatIcon('#HEX_COLOR'));
 
 console.log(window.SmocConfig);
-// Add the icon to the document body
-document.body.appendChild(createChatIcon('#HEX_COLOR'));
