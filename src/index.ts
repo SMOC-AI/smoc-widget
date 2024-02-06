dc10b8b6f4a7f67c53e0405e25273409b0b9937e
Alexander Bazo <alexbazo@live.no>
2024-02-06 21:15:25 +0100

    Added svg icon to widget

--- src/index.ts
+++ src/index.ts
@@ -2,37 +2,24 @@ document.addEventListener('DOMContentLoaded', function() {
     function createChatIcon() {
         const iconElement = document.createElement('div');
         iconElement.id = 'smoc-chat-icon';
-  
-        // Create an img element for the icon
-        const iconImg = document.createElement('img');
-        iconImg.src = './public/icons8-bubble-chat-25.png'; // Adjust the path to the image in the "public" directory
-        iconImg.style.width = '100%'; // Use 100% to ensure it fills the container div
-        iconImg.style.height = 'auto'; // Maintain aspect ratio
-        // Access custom properties using type assertion
-        const hexColor = (window as any).Color || '#ffeeed'; // Default color if undefined
-        const shape = (window as any).Shape || 'round'; // Default shape if undefined
-        const position = (window as any).Position || 'bottom-right'; // Default position
-  
+
+        
+        const hexColor = (window as any).Color || '#FFFFFF'; 
         iconElement.style.backgroundColor = hexColor;
-        if (shape === 'round') {
-            iconElement.style.borderRadius = '50%';
-        }
-  
-        // Set position
-        iconElement.style.position = 'fixed'; // Use fixed positioning to place the icon
+
+       
+        iconElement.style.position = 'fixed';
+        const position = (window as any).Position || 'bottom-right'; 
         if (position === 'bottom-left') {
             iconElement.style.bottom = '64px';
             iconElement.style.left = '64px';
-        } else { // Default to bottom-right
+        } else { 
             iconElement.style.bottom = '64px';
             iconElement.style.right = '64px';
         }
-  
-        // Append the icon image to the iconElement
-        iconElement.appendChild(iconImg);
-  
+
         return iconElement;
     }
-  
+
     document.body.appendChild(createChatIcon());
-  });
+});
